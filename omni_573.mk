4888099f3003a0c0154ff10764ba6cd1ec26c438
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-06-04 11:31:13 -0700

    Update omni_573.mk

--- omni_573.mk
+++ omni_573.mk
@@ -23,16 +23,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from Infinix-X573 device
-$(call inherit-product, device/infinix/X573/device.mk)
+$(call inherit-product, device/infinix/Infinix-X573/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := X573
-PRODUCT_NAME := omni_X573
+PRODUCT_DEVICE := Infinix-X573
+PRODUCT_NAME := omni_Infinix-X573
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X573
 PRODUCT_MANUFACTURER := infinix
-PRODUCT_RELEASE_NAME := Infinix Infinix X573
+PRODUCT_RELEASE_NAME := Infinix-X573
