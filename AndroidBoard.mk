a7be2b7cb689ddd528ea6e473806b14ba9120a99
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-06-05 22:40:11 +0000

    Update and rename Android.mk to AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -18,6 +18,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE), X573)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+ifeq ($(filter X573,$(TARGET_DEVICE)),)
+include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
