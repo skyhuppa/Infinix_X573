a2039bf8f576ba1cedd9e9c311701d8f54952c9b
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-06-14 21:10:33 +0000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,9 +81,9 @@ TARGET_KERNEL_CONFIG := Infinix_X573_defconfig
 #AVB: support chain partition signing. The following build variables are required to enable chain partition for recovery partition
 BOARD_AVB_ENABLE := true
 BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_USES_FULL_RECOVERY_IMAGE := true
-PRODUCT_SUPPORTS_BOOT_SIGNER := true
-PRODUCT_SUPPORTS_RECOVERY_SIGNER := true
+#BOARD_USES_FULL_RECOVERY_IMAGE := true
+#PRODUCT_SUPPORTS_BOOT_SIGNER := true
+#PRODUCT_SUPPORTS_RECOVERY_SIGNER := true
 #PRODUCT_VERITY_SIGNING_KEY := build/target/product/security/verity
 
 # Ramdisk compression
