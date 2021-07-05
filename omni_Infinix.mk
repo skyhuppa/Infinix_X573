5aa3ab3f1d16dc35db33584ea718a9712c249dab
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-07-05 18:37:59 +0000

    Update omni_Infinix.mk

--- omni_Infinix.mk
+++ omni_Infinix.mk
@@ -27,7 +27,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Infinix-X573
+PRODUCT_DEVICE := Infinix_X573
 PRODUCT_NAME := omni_Infinix
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X573
