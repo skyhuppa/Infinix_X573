0e5dfc828b961a51552d9590f0e099b8f0756dbe
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-11-21 04:36:18 -1200

    Update and rename omni_Infinix_X573.mk to twrp_Infinix_X573.mk

--- twrp_Infinix_X573.mk
+++ twrp_Infinix_X573.mk
@@ -22,13 +22,13 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Infinix_X573
-PRODUCT_NAME := omni_Infinix_X573
+PRODUCT_NAME := wrp_Infinix_X573
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X573
 PRODUCT_MANUFACTURER := Infinix Mobility Limited
