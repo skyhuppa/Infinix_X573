954d7acfb7d1333c06fa8639846ef88aacb16ef1
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-12-05 09:49:26 +0000

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -18,6 +18,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE), Infinix_X573)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+ifeq ($(filter Infinix_X573,$(TARGET_DEVICE)),)
+include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
