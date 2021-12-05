3ac9c5710a1d5cda860883184156d2941f7ace8d
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-12-05 17:15:00 +0000

    Update and rename twrp_Infinix X573.mk to twrp_Infinix_X573.mk

--- twrp_Infinix_X573.mk
+++ twrp_Infinix_X573.mk
@@ -17,8 +17,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
  	# $(LOCAL_PATH)/prebuilt/dtb:dtb.img
  
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Infinix X573
-PRODUCT_NAME := twrp_Infinix X573
+PRODUCT_DEVICE := Infinix_X573
+PRODUCT_NAME := twrp_Infinix_X573
 PRODUCT_BRAND := infinix
 PRODUCT_MODEL := Hot S 3
 PRODUCT_MANUFACTURER := infinix
