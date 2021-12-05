d3cc86353c46444647fc10d7c164264943254aeb
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-12-05 07:07:37 +0000

    Update and rename twrp_Infinix_X573.mk to twrp_Infinix-X573.mk

--- twrp_Infinix-X573.mk
+++ twrp_Infinix-X573.mk
@@ -26,8 +26,8 @@ $ (call inherit-product, vendor/twrp/config/common.mk)
 # $(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Infinix_X573
-PRODUCT_NAME := twrp_Infinix_X573
+PRODUCT_DEVICE := Infinix-X573
+PRODUCT_NAME := twrp_Infinix-X573
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix Hot S 3
 PRODUCT_MANUFACTURER := INFINIX MOBILITY LIMITED
