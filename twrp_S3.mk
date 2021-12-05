1047a1736054f7ba204458d48eba56fb4463e2ca
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-12-05 15:30:10 +0000

    Update and rename twrp_RMX3085.mk to twrp_S3.mk

--- twrp_S3.mk
+++ twrp_S3.mk
@@ -13,15 +13,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Inherit some common Omni stuff.
  $(call inherit-product, vendor/twrp/config/common.mk)
 
-  PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
- 	 $(LOCAL_PATH)/prebuilt/dtb:dtb.img
+  # PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
+ 	# $(LOCAL_PATH)/prebuilt/dtb:dtb.img
  
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := RMX3085
-PRODUCT_NAME := twrp_RMX3085
-PRODUCT_BRAND := realme
-PRODUCT_MODEL := rralme 8
-PRODUCT_MANUFACTURER := realme
+PRODUCT_DEVICE := S3
+PRODUCT_NAME := twrp_S3
+PRODUCT_BRAND := infinix
+PRODUCT_MODEL := Hot S 3
+PRODUCT_MANUFACTURER := infinix
 
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
