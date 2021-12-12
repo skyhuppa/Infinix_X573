bdf2f476fb3e72fa1e441a18f6b23bdf9aeb90f6
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-12-12 17:30:58 +0000

    Update omni_HotX573.mk

--- omni_HotX573.mk
+++ omni_HotX573.mk
@@ -8,7 +8,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit some common Omni stuff.
  $(call inherit-product, vendor/omni/config/common.mk)
