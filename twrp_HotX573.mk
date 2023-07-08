23bd7bf639fc784f4c66d5bf34072deedac89d07
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-07-08 14:49:55 -0700

    Update twrp_HotX573.mk

--- twrp_HotX573.mk
+++ twrp_HotX573.mk
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
  $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from mojito device
-$(call inherit-product, device/xiaomi/mojito/device.mk)
+$(call inherit-product, device/infinix/HotX573/device.mk)
 
 # PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
 #	$(LOCAL_PATH)/prebuilt/dtb:dtb.img
