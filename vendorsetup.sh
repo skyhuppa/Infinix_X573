9abd948c6db1f9903db5187cec3ed894ee0f6fcd
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-07-08 16:28:02 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,7 +17,7 @@
 #
 # 	Please maintain this if you use this script or any part of it
 #
-FDEVICE="HotX573"
+FDEVICE="Infinix-X573"
 #set -o xtrace
 
 fox_get_target_device() {
@@ -38,10 +38,10 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
    	export TW_DEFAULT_LANGUAGE="en"
 	export LC_ALL="C"
  	export ALLOW_MISSING_DEPENDENCIES=true
-  	export TARGET_DEVICE_ALT="HotX573"
-#	export FOX_TARGET_DEVICES="HotX573"
-	export FOX_VIRTUAL_AB_DEVICE=1
-	export FOX_AB_DEVICE=1
+  	export TARGET_DEVICE_ALT="Infinix-X573"
+#	export FOX_TARGET_DEVICES="Infinix-X573"
+#	export FOX_VIRTUAL_AB_DEVICE=1
+#	export FOX_AB_DEVICE=1
   
 	# export OF_KEEP_FORCED_ENCRYPTION=1
         export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
