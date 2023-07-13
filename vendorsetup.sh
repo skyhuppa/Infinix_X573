9765b57647097a2cd72a06b652dc83daebeeae57
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-07-13 09:39:54 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -90,7 +90,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	# -- add settings for R11 --
         export OF_MAINTAINER=Skyhuppa
         export FOX_BUILD_TYPE="Stable"
-        export FOX_VARIANT=A13
+        export FOX_VARIANT=A12.1
 	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
 	export OF_QUICK_BACKUP_LIST="/boot;/data;"
 	# -- end R11 settings --
