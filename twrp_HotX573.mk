be983971b876034334559b19baa2eaf402ef5ab6
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-09-15 05:38:11 +1000

    Update twrp_HotX573.mk

--- twrp_HotX573.mk
+++ twrp_HotX573.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 
 # Inherit some common Omni stuff.
- $(call inherit-product, vendor/pb/config/common.mk)
+ $(call inherit-product, vendor/twrp/config/common.mk)
 
 
 # PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/infinic/HotX573/recovery/root,recovery/root)
