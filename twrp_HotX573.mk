2164434d5a2be18803cb73913ea25cea48919611
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-09-15 02:46:19 +1000

    Update twrp_HotX573.mk

--- twrp_HotX573.mk
+++ twrp_HotX573.mk
@@ -8,7 +8,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-# $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit Target Product stuff
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
