b25969234f1b97086a51b724b0d5919af8f713b4
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-09-23 14:14:16 -0700

    Update twrp_HotX573.mk

--- twrp_HotX573.mk
+++ twrp_HotX573.mk
@@ -16,12 +16,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
  $(call inherit-product, vendor/twrp/config/common.mk)
 
 
-# PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/infinic/HotX573/recovery/root,recovery/root)
+# PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/infinix/Infinix_X573/recovery/root,recovery/root)
 # $(LOCAL_PATH)/prebuilt/dtb:dtb.img
  
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := HotX573
-PRODUCT_NAME := twrp_HotX573
+PRODUCT_NAME := twrp_Infinix_X573
 PRODUCT_BRAND := X573
 PRODUCT_MODEL := Hot S 3
 PRODUCT_MANUFACTURER := INFINIX MOBILITY LIMITED
