cf92d6398fcbe060131a24c193ee78b6ed2c03c4
Adasi Kaakyire <Skyhuppa@gmail.com>
2025-06-28 02:20:07 +0000

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,7 +6,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_Infinix_X573.mk
+    $(LOCAL_DIR)/pb_Infinix_X573.mk
 
     COMMON_LUNCH_CHOICES := \
-       twrp_Infinix_X573-eng
+       pb_Infinix_X573-eng
