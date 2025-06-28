583c0f53b551c0920a019b889b292b207daa730a
Adasi Kaakyire <Skyhuppa@gmail.com>
2025-06-28 16:04:55 +0000

    Update pb_Infinix_X573.mk

--- pb_Infinix_X573.mk
+++ pb_Infinix_X573.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit some common PBRP stuff.
 $(call inherit-product, vendor/pb/config/common.mk)
 
+# Inherit device configuration
+$(call inherit-product, device/Infinix/Infinix_X573/device.mk)
+
 # PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/infinix/Infinix_X573/recovery/root,recovery/root)
 # $(LOCAL_PATH)/prebuilt/dtb:dtb.img
  
