2aae6ef6972e8c0fe15144a194d55ad5a4cfdd0d
Adasi Kaakyire <Skyhuppa@gmail.com>
2025-06-28 15:44:06 +0000

    Update pb_Infinix_X573.mk

--- pb_Infinix_X573.mk
+++ pb_Infinix_X573.mk
@@ -5,9 +5,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Release name
+# PRODUCT_RELEASE_NAME := Infinix_X573
+
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit Target Product stuff
 # $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
