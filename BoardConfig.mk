4d92d5d04e2add9524aebde480e4be83558bb493
Adasi Kaakyire <Skyhuppa@gmail.com>
2025-10-16 06:57:50 +0000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,8 @@ DEVICE_PATH := device/infinix/Infinix_X573
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 
 # Architecture
 TARGET_ARCH := arm64
